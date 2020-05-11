08cc3a0043d786df23f0b0d483b29e44fe1bb32d
Zans <zans.litinskis@gmail.com>
2020-05-11 20:43:59 +0100

    CHAT-0003 Confirm page

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable prefer-promise-reject-errors */
 import React, { useState } from 'react';
-import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
+import {
+  UserOutlined, LockOutlined, MailOutlined, InfoCircleTwoTone,
+} from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { Form, Input } from 'antd';
 import Button from '../../../components/Button';
@@ -15,6 +17,7 @@ export default function RegisterForm() {
       setNameValid('error');
     }
   };
+  const success = true;
   return (
     <>
       <div className="auth__top">
@@ -22,78 +25,86 @@ export default function RegisterForm() {
         <p>Для входа в чат, вам нужно зарегистрироваться</p>
       </div>
       <Block>
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
-        >
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: 'Please input your Email!' }]}
-            validateStatus={nameIsValid}
-            hasFeedback
+        {!success ? (
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
           >
-            <Input prefix={<MailOutlined className="site-form-item-icon" />} type="email" placeholder="E-mail" size="large" onChange={(event) => onFinish(event.target.value)} />
-          </Form.Item>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: 'Please input your name!' }]}
-          >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Name"
-              size="large"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input
-              type="password"
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              placeholder="Password"
-              size="large"
-              autoComplete="password"
-            />
-          </Form.Item>
-          <Form.Item
-            name="confirm"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password!',
-              },
-              ({ getFieldValue }) => ({
-                validator(rule, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject('The two passwords that you entered do not match!');
+            <Form.Item
+              name="email"
+              rules={[{ required: true, message: 'Please input your Email!' }]}
+              validateStatus={nameIsValid}
+              hasFeedback
+            >
+              <Input prefix={<MailOutlined className="site-form-item-icon" />} type="email" placeholder="E-mail" size="large" onChange={(event) => onFinish(event.target.value)} />
+            </Form.Item>
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: 'Please input your name!' }]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Name"
+                size="large"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input
+                type="password"
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Password"
+                size="large"
+                autoComplete="password"
+              />
+            </Form.Item>
+            <Form.Item
+              name="confirm"
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
                 },
-              }),
-            ]}
-          >
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject('The two passwords that you entered do not match!');
+                  },
+                }),
+              ]}
+            >
 
-            <Input
-              type="password"
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              placeholder="Confirm password"
-              size="large"
-              autoComplete="password"
-            />
-          </Form.Item>
+              <Input
+                type="password"
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Confirm password"
+                size="large"
+                autoComplete="password"
+              />
+            </Form.Item>
 
 
-          <Form.Item>
-            <Button size="large" type="primary" className="login-form-button" htmlType="submit">Зарегистрироваться</Button>
-          </Form.Item>
-          <Form.Item className="login-form-register">
-            <Link to="/login">Войти в аккаунт</Link>
-          </Form.Item>
-        </Form>
+            <Form.Item>
+              <Button size="large" type="primary" className="login-form-button" htmlType="submit">Зарегистрироваться</Button>
+            </Form.Item>
+            <Form.Item className="login-form-register">
+              <Link to="/login">Войти в аккаунт</Link>
+            </Form.Item>
+          </Form>
+        ) : (
+          <div className="auth__success">
+            <InfoCircleTwoTone className="info-icon" style={{ fontSize: '48px' }} />
+            <h2>Подтвердите свой аккаунт</h2>
+            <p>На вашу почту отправлено пиьсмо с ссылкой на подтверждение аккаунта.</p>
+          </div>
+        )}
 
       </Block>
     </>
