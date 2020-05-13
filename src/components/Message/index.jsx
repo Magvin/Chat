2a32caa43db4903e930bd46c99c1f5b356eaff0a
Zans <zans.litinskis@gmail.com>
2020-05-13 21:08:16 +0100

    CHAT-0005 Read and Not readed message svg

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -3,28 +3,35 @@ import PropTypes from 'prop-types';
 import formatDistanceToNow from 'date-fns/esm/formatDistanceToNow';
 import eoLocale from 'date-fns/locale/ru';
 import classnames from 'classnames';
+import readed from '../../assets/img/readed.svg';
+import noReaded from '../../assets/img/noreaded.svg';
 
 // CSS
 import './Message.scss';
 
 const Message = ({
-  avatar, user, text, date,
+  avatar, user, text, date, isMe, isReaded,
 }) => {
   const formatDate = (d) => formatDistanceToNow(
     new Date(d), { locale: eoLocale, addSuffix: true },
   );
   return (
-    <div className="message">
+    <div className={classnames('message', { 'message--isme': isMe })}>
       <div className="message__avatar">
         <img src={avatar} alt={`Avatar of ${user.fullname}`} />
       </div>
       <div className="message__content">
-        <div className="message__bubble">
-          <p className="message__text">{text}</p>
+        <div className="message__info">
+          <div className="message__bubble">
+            <p className="message__text">{text}</p>
+          </div>
+          {(isMe && isReaded) ? <img src={readed} alt="message readed" /> : <img src={noReaded} alt="message is not readed" /> }
+          <span className="message__date">
+            {formatDate(date)}
+          </span>
         </div>
-        <span className="message__date">
-          {formatDate(date)}
-        </span>
+
+
       </div>
     </div>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,19 @@ import { Message } from '../../components';
 export default function Home() {
   return (
     <section className="home">
-      <Message avatar="https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1090&q=80" text="Салам, Брут!" date="Tue May 12 2020 21:16:22" />
+      <Message
+        avatar="https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1090&q=80"
+        text="Салам, Брут!"
+        date="Tue May 12 2020 21:16:22"
+        isMe={false}
+      />
+      <Message
+        avatar="https://wi-images.condecdn.net/image/jgov7eBrRvb/crop/2040/f/6-facial-recognition-hero.jpg"
+        text="Салам, Брут!"
+        date="Tue May 12 2020 22:16:22"
+        isMe={true}
+        isReaded={false}
+      />
     </section>
   );
 }
