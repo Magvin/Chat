365c5479c2665a69c6d5b3ebbe7c40f290ec6958
zans.litinskis@gmail.com <zans.litinskis@gmail.com>
2020-05-27 21:21:23 +0100

    CHAT-00013 Lodash plugged in

--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
+import OrderBy from 'lodash/orderBy';
 import { ContactListItem } from '..';
 
 
-const ContactList = ({ list, ownerId }) => {
-  console.log(list);
-  return list && list.map(({
-    user, message, _id,
-  }) => (
-    <ContactListItem
-      isOnline={user.isOnline}
-      user={user}
-      key={_id}
-      message={message}
-      isMe={user.id === ownerId}
-    />
-  ));
-};
+const ContactList = ({ list, ownerId }) => (OrderBy(list, ['message.created_at'], ['asc']).map(({
+  user, message, _id,
+}) => (
+  <ContactListItem
+    isOnline={user.isOnline}
+    user={user}
+    key={_id}
+    message={message}
+    isMe={user.id === ownerId}
+  />
+)));
 
 
 export default ContactList;
